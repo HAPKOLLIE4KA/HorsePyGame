83134144e84db624423d6f36ef60d9f3cfbaddb9
HAPKOLLIE4KA <100372994+HAPKOLLIE4KA@users.noreply.github.com>
2022-05-30 02:20:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,53 +19,20 @@ def main():
     story_moves = []
 
     running = True
-    click_start_position = False # начальная позиция выбрана или нет
-    auto_move = False # автоходы
-    tapLeft = False # нажатие на стрелку влево
+    click_start_position = False  # начальная позиция выбрана или нет
+    auto_move = False  # автоходы
+    tapLeft = False  # нажатие на стрелку влево
     next_move = False  # следующий ход по конпке
 
-    while running:
+    while not click_start_position:
 
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 running = False
+                click_start_position = True
 
-            elif click_start_position and (event.type == pygame.KEYDOWN):  # управление клавишами
-                if event.key == pygame.K_SPACE:
-                    FPS = 2
-                    auto_move = not auto_move
-
-                if event.key == pygame.K_RIGHT:
-                    FPS = 10
-                    auto_move = False
-                    next_move = True
-
-                if event.key == pygame.K_LEFT:
-                    FPS = 10
-                    auto_move = False
-                    tapLeft = True
-
-                if event.key == pygame.K_UP:
-                    FPS += 1
-
-                    if FPS > 6: FPS = 6
-
-                if event.key == pygame.K_DOWN:
-                    FPS -= 1
-                    if FPS < 2: FPS = 2
-
-                if event.key == pygame.K_RETURN:
-                    FPS = 150
-
-            elif click_start_position and event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                    tapLeft = False
-                    next_move = False
-
-
-            elif not click_start_position and event.type == pygame.MOUSEBUTTONDOWN:  # установка изначальной позиции
-
+            if event.type == pygame.MOUSEBUTTONDOWN:  # установка изначальной позиции
                 cursor_position = event.pos
 
                 for row in range(COUNT_CELL_IN_RAW):
@@ -78,26 +45,68 @@ def main():
                             position_x, position_y = column, row
                             click_start_position = True
 
+        screen.fill(GRAY)
+        print_board(cells, story_moves, screen)
 
-        if click_start_position and tapLeft and len(story_moves) > 1:
+        clock.tick(FPS)
+        pygame.display.flip()
+
+    while running:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+            elif (event.type == pygame.KEYDOWN):  # управление клавишами
+                if event.key == pygame.K_SPACE:
+                    FPS = 2
+                    auto_move = not auto_move
+
+                if event.key == pygame.K_RIGHT:  # нажатие на стрелку вправо
+                    FPS = 8
+                    auto_move = False
+                    next_move = True
+
+                if event.key == pygame.K_LEFT:  # нажатие на стрелку влево
+                    FPS = 10
+                    auto_move = False
+                    tapLeft = True
+
+                if event.key == pygame.K_UP:  # ускорение для автохода
+                    FPS += 1
+
+                    if FPS > 6: FPS = 6
+
+                if event.key == pygame.K_DOWN:
+                    FPS -= 1
+                    if FPS < 2: FPS = 2
+
+                if event.key == pygame.K_RETURN:  # переход в самый конец
+                    FPS = 150
+
+            elif event.type == pygame.KEYUP:
+
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
+                    tapLeft = False
+                    next_move = False
+
+        if tapLeft and len(story_moves) > 1:  # ход назад
             moves[position_y][position_x] = 0
             position_x, position_y = story_moves[-2].get_coords_matrix
             story_moves.pop()
 
-        screen.fill(GRAY)
-
-        print_board(cells, story_moves, screen)
-
-        if click_start_position:
-            screen.blit(horse_image, cells[position_y][position_x].get_horse_coords)
-
-        if auto_move or next_move:
+        if auto_move or next_move:  # следующий ход коня
 
             x, y = new_move(moves, position_x, position_y)
             if (x, y) != (-1000, -1000):
                 position_x, position_y = x, y
                 moves[position_y][position_x] = 1
                 story_moves += [cells[position_y][position_x]]
+
+        screen.fill(GRAY)  # отрисовка
+        print_board(cells, story_moves, screen)
+        screen.blit(horse_image, cells[position_y][position_x].get_horse_coords)
 
         clock.tick(FPS)
         pygame.display.flip()
